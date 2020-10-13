0cb18b7a7a09f9d1f3e884fe57330f0a7455a787
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2020-10-13 10:07:16 +0200

    Load decidim-courses as module and add menu items (#3)
    * Add module to Gemfile
    * Convert to module from component
    * Add admin and public menu item
    * Change menu icon
    * Update decidim-courses/lib/decidim/courses/participatory_space.rb
    Co-authored-by: victorol1 <56717126+victorol1@users.noreply.github.com>
    * Update decidim-courses/lib/decidim/courses/participatory_space.rb
    Co-authored-by: victorol1 <56717126+victorol1@users.noreply.github.com>
    * Remove export block
    * Fix trailing character typo
    Co-authored-by: victorol1 <56717126+victorol1@users.noreply.github.com>

--- decidim-courses/lib/decidim/courses/admin_engine.rb
+++ decidim-courses/lib/decidim/courses/admin_engine.rb
@@ -10,13 +10,18 @@ module Decidim
       paths["lib/tasks"] = nil
 
       routes do
-        # Add admin engine routes here
-        # resources :courses do
-        #   collection do
-        #     resources :exports, only: [:create]
-        #   end
-        # end
-        # root to: "courses#index"
+        resources :courses, param: :slug, except: [:show, :destroy]
+      end
+
+      initializer "decidim_courses.admin_menu" do
+        Decidim.menu :admin_menu do |menu|
+          menu.item I18n.t("menu.courses", scope: "decidim.admin"),
+                    decidim_admin_courses.courses_path,
+                    icon_name: "book",
+                    position: 3.5,
+                    active: :inclusive
+                    # TODO: if: allowed_to?(:enter, :space_area, space_name: :courses)
+        end
       end
 
       def load_seed
---
+++ decidim-courses/lib/decidim/courses/participatory_space.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+Decidim.register_participatory_space(:courses) do |participatory_space|
+  participatory_space.icon = "decidim/courses/icon.svg"
+  participatory_space.model_class_name = "Decidim::Courses::Course"
+
+  participatory_space.participatory_spaces do |organization|
+    Decidim::Course.where(organization: organization)
+  end
+
+  participatory_space.permissions_class_name = "Decidim::Courses::Permissions"
+
+  # participatory_space.query_type = "Decidim::Courses::CourseType"
+
+  participatory_space.register_resource(:course) do |resource|
+    resource.model_class_name = "Decidim::Course"
+    # resource.card = "decidim/courses/course"
+    # resource.searchable = true
+  end
+
+  participatory_space.context(:public) do |context|
+    context.engine = Decidim::Courses::Engine
+    # context.layout = "layouts/decidim/course"
+  end
+
+  participatory_space.context(:admin) do |context|
+    context.engine = Decidim::Courses::AdminEngine
+    # context.layout = "layouts/decidim/admin/course"
+  end
+
+  participatory_space.register_on_destroy_account do |user|
+    # TODO
+  end
+
+  participatory_space.seeds do
+    # TODO
+  end
+end
