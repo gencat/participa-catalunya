909ddd33f52d49e18f0a3312e1781a4199f24ab3
victorol1 <56717126+victorol1@users.noreply.github.com>
2020-11-10 11:37:29 +0100

    Update decidim-resource_banks/lib/decidim/resource_banks/version.rb
    Co-authored-by: Rubén González <2051199+rbngzlv@users.noreply.github.com>

--- decidim-resource_banks/lib/decidim/resource_banks/version.rb
+++ decidim-resource_banks/lib/decidim/resource_banks/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This holds the decidim-meetings version.
+  # This holds the decidim-resource_banks version.
   module ResourceBanks
     def self.version
       "0.22.0"
