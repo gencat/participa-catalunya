3ce0efdd571a85430adff4e9c770514c4bb2dcd6
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2020-11-17 10:11:24 +0100

    Resource banks: init specs, factories and model spec

--- decidim-resource_banks/lib/decidim/resource_banks/test/factories.rb
+++ decidim-resource_banks/lib/decidim/resource_banks/test/factories.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
 require "decidim/core/test/factories"
+require "decidim/faker/localized"
+require "decidim/dev"
 
 FactoryBot.define do
-  factory :resource_bank_component, parent: :component do
-    name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :resource_bank).i18n_name }
-    manifest_name :resource_banks
-    participatory_space { create(:participatory_process, :with_steps) }
+  sequence(:resource_bank_slug) do |n|
+    "#{Faker::Internet.slug(nil, "-")}-#{n}"
   end
 
-  # Add engine factories here
+  factory :resource_bank, class: "Decidim::ResourceBank" do
+    title { generate_localized_title }
+    slug { generate(:resource_bank_slug) }
+    text { Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title } }
+    authorship { Faker::Name.name }
+    video_url { Faker::Internet.url }
+    organization
+    hero_image { Decidim::Dev.test_file("city.jpeg", "image/jpeg") } # Keep after organization
+    banner_image { Decidim::Dev.test_file("city2.jpeg", "image/jpeg") } # Keep after organization
+    published_at { Time.current }
+    hashtag { generate(:hashtag_name) }
+    show_statistics { true }
+
+    trait :promoted do
+      promoted { true }
+    end
+
+    trait :unpublished do
+      published_at { nil }
+    end
+
+    trait :published do
+      published_at { Time.current }
+    end
+  end
 end
