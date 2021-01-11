0c8f2a06657e6aa672ce017373e2da37da5e2824
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2021-01-11 03:35:53 +0100

    Courses list card

--- decidim-courses/app/cells/decidim/courses/course_m_cell.rb
+++ decidim-courses/app/cells/decidim/courses/course_m_cell.rb
@@ -8,38 +8,23 @@ module Decidim
       include Decidim::ViewHooksHelper
       include ActionView::Helpers::DateHelper
 
+      delegate :duration, :schedule, to: :model
+
       private
 
-      def has_image? # rubocop:disable Naming/PredicateName
-        true
+      def description
+      end
+
+      def statuses
+        []
+      end
+
+      def modality
+        t(model.modality, scope: "decidim.courses.modality") if model.modality
       end
 
       def resource_path
         Decidim::Courses::Engine.routes.url_helpers.course_path(model)
-      end
-
-      def resource_image_path
-        model.hero_image.url
-      end
-
-      def statuses
-        [:start_date, :duration, :modality]
-      end
-
-      def start_date_status
-        l(model.start_date, format: :decidim_short) if model.start_date
-      end
-
-      def duration_status
-        model.duration
-      end
-
-      def modality_status
-        t(model.modality, scope: "decidim.courses.modality") if model.modality
-      end
-
-      def resource_icon
-        icon "courses", class: "icon--big"
       end
     end
   end
