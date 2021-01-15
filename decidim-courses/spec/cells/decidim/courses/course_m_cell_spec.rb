5884123009afd7a8319f1962c73f30716774c79e
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2021-01-15 20:28:34 +0100

    Update specs

--- decidim-courses/spec/cells/decidim/courses/course_m_cell_spec.rb
+++ decidim-courses/spec/cells/decidim/courses/course_m_cell_spec.rb
@@ -21,6 +21,26 @@ module Decidim::Courses
       it "shows the course modality" do
         expect(subject).to have_content(t(model.modality, scope: "decidim.courses.modality"))
       end
+
+      it "shows the course dates" do
+        within "card__course-date" do
+          expect(subject).to have_content(I18n.l(model.start_date.to_date, format: :decidim_short_with_month_name_short))
+          expect(subject).to have_content(I18n.l(model.end_date.to_date, format: :decidim_short_with_month_name_short))
+        end
+      end
+
+      context "when course does not spans multiple days" do
+        before do
+          model.end_date = model.start_date
+        end
+
+        it "renders only one date" do
+          within ".card__course-date" do
+            expect(subject).to have_content(I18n.l(model.start_date.to_date, format: :decidim_short_with_month_name_short), count: 1)
+            expect(subject).not_to have_content("-")
+          end
+        end
+      end
     end
   end
 end
