f4b03f05a9fb5c66284dc719a54c6fa17d45b115
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2021-01-15 20:08:18 +0100

    Use start / end dates instead of schedule field

--- decidim-courses/app/cells/decidim/courses/course_m_cell.rb
+++ decidim-courses/app/cells/decidim/courses/course_m_cell.rb
@@ -8,7 +8,7 @@ module Decidim
       include Decidim::ViewHooksHelper
       include ActionView::Helpers::DateHelper
 
-      delegate :duration, :schedule, to: :model
+      delegate :duration, to: :model
 
       private
 
@@ -21,6 +21,24 @@ module Decidim
 
       def modality
         t(model.modality, scope: "decidim.courses.modality") if model.modality
+      end
+
+      def spans_multiple_dates?
+        start_date != end_date
+      end
+
+      def course_date
+        return render(:multiple_dates) if spans_multiple_dates?
+
+        render(:single_date)
+      end
+
+      def start_date
+        model.start_date.to_date
+      end
+
+      def end_date
+        model.end_date.to_date
       end
 
       def resource_path
