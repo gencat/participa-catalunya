1b7173b6c197e1076359cad46a8d3d0b866bfc94
Víctor <victor.ol@coditramuntana.com>
2021-01-22 12:46:58 +0100

    Fix rubocop offenses

--- db/migrate/20210122113309_add_descriptive_fields_to_courses.decidim_courses.rb
+++ db/migrate/20210122113309_add_descriptive_fields_to_courses.decidim_courses.rb
@@ -3,7 +3,7 @@
 
 class AddDescriptiveFieldsToCourses < ActiveRecord::Migration[5.2]
   def change
-      change_table :decidim_courses do |t|
+    change_table :decidim_courses do |t|
       t.column :objectives, :jsonb
       t.column :addressed_to, :jsonb
       t.column :programme, :jsonb
