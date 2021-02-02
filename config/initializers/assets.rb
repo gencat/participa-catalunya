96f8449204d32b0364ddabc29da4ddb1c4ccbf79
Víctor <victor.ol@coditramuntana.com>
2021-02-02 12:15:03 +0100

    Run rubocop

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,5 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( datepicker-locales/foundation-datepicker.oc.js )
-Rails.application.config.assets.precompile += %w( logo_generalitat_gris.png )
+Rails.application.config.assets.precompile += %w(datepicker-locales/foundation-datepicker.oc.js)
+Rails.application.config.assets.precompile += %w(logo_generalitat_gris.png)
