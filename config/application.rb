07f118fb20369fc3ddee608f1bb5d7718f282a73
Víctor <victor.ol@coditramuntana.com>
2021-02-03 10:27:18 +0100

    Run delayed_jobs migrations and add missing config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,16 @@ module ParticipaCatalunya
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Custom I18n fallbacks
+    config.after_initialize do
+      I18n.fallbacks = I18n::Locale::Fallbacks.new(
+        {
+          ca: [:ca, :es, :en],
+          es: [:es, :ca, :en],
+          oc: [:oc, :ca, :es, :en]
+        }
+      )
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
