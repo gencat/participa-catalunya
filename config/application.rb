824f01a7fd681ae92d051ca8bb3878e3843baf7e
Víctor <victor.ol@coditramuntana.com>
2021-02-03 10:38:23 +0100

    Run rubocop

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,9 @@ module ParticipaCatalunya
     # Custom I18n fallbacks
     config.after_initialize do
       I18n.fallbacks = I18n::Locale::Fallbacks.new(
-        {
-          ca: [:ca, :es, :en],
-          es: [:es, :ca, :en],
-          oc: [:oc, :ca, :es, :en]
-        }
+        ca: [:ca, :es, :en],
+        es: [:es, :ca, :en],
+        oc: [:oc, :ca, :es, :en]
       )
     end
     # Settings in config/environments/* take precedence over those specified here.
