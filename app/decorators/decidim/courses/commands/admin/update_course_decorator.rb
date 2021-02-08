749830b6eca9549353b57a7dec50c4f75cc386df
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2021-02-08 16:25:01 +0100

    Courses adapt to department admin

---
+++ app/decorators/decidim/courses/commands/admin/update_course_decorator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Intercepts the `call` method and forces the Area of the user if it is a
+# department_admin user.
+Decidim::Courses::Admin::UpdateCourse.class_eval do
+  alias_method :original_call, :call
+
+  def call
+    if form.current_user.department_admin?
+      form.area_id = form.current_user.areas.first.id
+    end
+
+    original_call
+  end
+end
---
+++ app/decorators/decidim/courses/queries/courses_with_user_role_decorator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+#
+# This decorator adds the capability to query courses
+# filtering by User role `department_admin`.
+#
+Decidim::Courses::CoursesWithUserRole.class_eval do
+  private
+
+  alias_method :course_ids_for_user_role, :course_ids
+
+  def course_ids
+    ids = course_ids_for_user_role
+
+    if user&.department_admin?
+      course_ids_for_department = ::Decidim::Course
+                                    .where(decidim_area_id: user.areas.pluck(:id))
+                                    .pluck(:id)
+
+      ids.concat(course_ids_for_department)
+    end
+
+    ids.uniq
+  end
+end
