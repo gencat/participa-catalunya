e08791297e5d77cd5f3f1d69fcf6a9b790d055fc
Víctor <victor.ol@coditramuntana.com>
2021-02-12 11:51:48 +0100

    Add course registrations migrations

---
+++ db/migrate/20210212101510_create_course_registrations.decidim_courses.rb
@@ -0,0 +1,18 @@
+# This migration comes from decidim_courses (originally 20210212095914)
+class CreateCourseRegistrations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :decidim_courses_course_registrations do |t|
+      t.references :decidim_user, null: false, index: { name: "index_decidim_courses_registrations_on_decidim_user_id" }
+      t.references :decidim_course, null: false, index: { name: "index_courses_registrations_on_decidim_course" }
+
+      t.timestamps
+      t.integer "decidim_course_registration_type_id"
+      t.datetime "confirmed_at"
+    end
+
+    add_index :decidim_courses_course_registrations,
+              [:decidim_user_id, :decidim_course_id],
+              unique: true,
+              name: "decidim_courses_registrations_user_course_unique"  
+  end
+end
---
+++ db/migrate/20210212101511_create_course_registration_types.decidim_courses.rb
@@ -0,0 +1,15 @@
+# This migration comes from decidim_courses (originally 20210212100430)
+class CreateCourseRegistrationTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :decidim_courses_course_registration_types do |t|
+      t.references :decidim_course, index: { name: "idx_registration_types_on_decidim_course_id" }
+      t.jsonb :title, null: false
+      t.jsonb :description, null: false
+      t.decimal :price, null: false, default: 0, precision: 8, scale: 2
+      t.integer :weight, null: false, default: 0
+      t.datetime :published_at, index: true
+
+      t.timestamps
+    end
+  end
+end
