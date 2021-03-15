0d6c39586f7a9e51bfee8954641dd727cfa2abb6
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-03-15 16:33:46 +0100

    Apply new styles to footer

--- app/overrides/layouts/decidim/_main_footer.rb
+++ app/overrides/layouts/decidim/_main_footer.rb
@@ -1,31 +1,4 @@
 
-feder_text= <<~EOFEDER
-<section class="footer__subhero extended subhero home-section">
-  <div class="row">
-    <div class="columns small-12 large-9">
-      <div class="row">
-        <div class="small-10 medium-12 columns small-centered large-uncentered m-bottom">
-          <%= t("decidim.pages.home.feder.description") %>
-        </div>
-      </div>
-    </div>
-    <div class="columns small-12 large-3 end">
-      <div class="row">
-        <div class="small-8 medium-6 large-12 columns small-centered ">
-          <%= link_to "http://www.accio.gencat.cat/ca/accio/agencia/feder/", target: "_blank", class: "part-logo-footer-feder" do %>
-            <%= image_tag "feder.png", alt: "FEDER" %>
-          <% end %>
-        </div>
-      </div>
-  </div>
-</section>
-EOFEDER
-Deface::Override.new(:virtual_path => "layouts/decidim/_main_footer", 
-                     :name => "feder_footer", 
-                     :insert_before => "div.main-footer", 
-                     :text => feder_text,
-                     original: "4acba79b4f172ee3abaeb8a2f5385440646a50a8")
-
 Deface::Override.new(:virtual_path => "layouts/decidim/_main_footer", 
                      :name => "remote_logo", 
                      :remove => "div.main-footer a.main-footer__badge", 
@@ -49,9 +22,42 @@ gencat_text= <<~GENCAT
     </div>
   </div>
 </div>
+<div class="row"><br /></div>
 GENCAT
+# Deface::Override.new(:virtual_path => "layouts/decidim/_main_footer", 
+#                      :name => "gencat_footer", 
+#                      :insert_bottom => "div.main-footer", 
+#                      :text => gencat_text,
+#                      original: "4acba79b4f172ee3abaeb8a2f5385440646a50a8")
 Deface::Override.new(:virtual_path => "layouts/decidim/_main_footer", 
-                     :name => "gencat_footer", 
-                     :insert_bottom => "div.main-footer", 
-                     :text => gencat_text,
+                     :name => "gencat_footer2", 
+                     :insert_after => "div.main-footer",
+                     :text => "<section>#{gencat_text}</section",
+                     original: "4acba79b4f172ee3abaeb8a2f5385440646a50a8")
+
+feder_text= <<~EOFEDER
+<section class="footer__subhero extended subhero home-section">
+  <div class="row">
+    <div class="columns small-12 large-9">
+      <div class="row">
+        <div class="small-10 medium-12 columns small-centered large-uncentered m-bottom">
+          <%= t("decidim.pages.home.feder.description") %>
+        </div>
+      </div>
+    </div>
+    <div class="columns small-12 large-3 end">
+      <div class="row">
+        <div class="small-8 medium-6 large-12 columns small-centered ">
+          <%= link_to "http://www.accio.gencat.cat/ca/accio/agencia/feder/", target: "_blank", class: "part-logo-footer-feder" do %>
+            <%= image_tag "feder.png", alt: "FEDER" %>
+          <% end %>
+        </div>
+      </div>
+  </div>
+</section>
+EOFEDER
+Deface::Override.new(:virtual_path => "layouts/decidim/_main_footer", 
+                     :name => "feder_footer", 
+                     :insert_after => "div.main-footer", 
+                     :text => feder_text,
                      original: "4acba79b4f172ee3abaeb8a2f5385440646a50a8")
