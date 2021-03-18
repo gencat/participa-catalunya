1dd1d7e6ae61e55189ad2572a02c3b2bed0ef827
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-03-18 17:19:47 +0100

    Rubocopify

--- config/initializers/department_admin.rb
+++ config/initializers/department_admin.rb
@@ -1,4 +1,6 @@
-Decidim::DepartmentAdmin::Permissions.configurable_checks= [
-  {permission_for?: [:admin, :enter, :space_area, space_name: :courses]},
-  {permission_for?: [:admin, :enter, :space_area, space_name: :resource_banks]},
+# frozen_string_literal: true
+
+Decidim::DepartmentAdmin::Permissions.configurable_checks = [
+  { permission_for?: [:admin, :enter, :space_area, space_name: :courses] },
+  { permission_for?: [:admin, :enter, :space_area, space_name: :resource_banks] }
 ]
