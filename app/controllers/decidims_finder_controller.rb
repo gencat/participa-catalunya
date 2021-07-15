9913e46cb94d033db9de84d8da0cde217bb20427
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-07-15 12:25:57 +0200

    Current translations is a decidim_finder's specific helper method

--- app/controllers/decidims_finder_controller.rb
+++ app/controllers/decidims_finder_controller.rb
@@ -3,5 +3,12 @@
 class DecidimsFinderController < Decidim::ApplicationController
   layout "layouts/decidim/application"
 
+  helper_method :current_translations
+
   def show; end
+
+  def current_translations
+    @translations ||= I18n.backend.send(:translations)
+    @translations[I18n.locale].with_indifferent_access
+  end
 end
