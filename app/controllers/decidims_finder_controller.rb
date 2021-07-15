47231eb611cc35b811e14e45273b79c81c34f092
David Pérez <david.perez3@dxcfds.com>
2021-07-15 12:09:03 +0200

    rubocop fix

--- app/controllers/decidims_finder_controller.rb
+++ app/controllers/decidims_finder_controller.rb
@@ -4,4 +4,9 @@ class DecidimsFinderController < Decidim::ApplicationController
   layout "layouts/decidim/application"
 
   def show; end
+
+  def current_translations
+    @translations ||= I18n.backend.send(:translations)
+    @translations[I18n.locale].with_indifferent_access
+  end
 end
